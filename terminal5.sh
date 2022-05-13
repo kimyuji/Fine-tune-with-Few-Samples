01af2090a1bab607703d2de7e2dbab8c87de3ee7
kimyuji <yujin301300@gmail.com>
2022-05-13 09:03:43 +0900

    [ADD]r/bf/workshop

--- terminal5.sh
+++ terminal5.sh
@@ -1,7 +1,7 @@
 for TARGET in "CropDisease" "EuroSAT" "ISIC" "ChestX" ; do
-  python ./finetune_full.py --ls --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr --ft_parts full --split_seed 1 --ft_intermediate_test --n_shot 1 --gpu_idx 1 --v_score --ft_mixup both --ft_scheduler_start 30 --ft_scheduler_end 70
+  python ./finetune_full.py --ls --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr --ft_parts full --split_seed 1 --ft_intermediate_test --n_shot 1 --gpu_idx 1 --v_score --ft_mixup both
 done
 
 for TARGET in "miniImageNet_test" "CropDisease" "EuroSAT" "ISIC" "ChestX" ; do
-  python ./finetune_full.py --ls --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr --ft_parts full --split_seed 1 --ft_intermediate_test --n_shot 1 --gpu_idx 5 --v_score --ft_mixup both --ft_scheduler_start 30 --ft_scheduler_end 70
+  python ./finetune_full.py --ls --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr --ft_parts full --split_seed 1 --ft_intermediate_test --n_shot 20 --gpu_idx 0 --v_score --ft_mixup both --ft_batch_size 16
 done
