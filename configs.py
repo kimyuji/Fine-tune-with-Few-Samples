680a4b6e1e1da3a18e6a3fbe96b09c30cb5ee656
kimyuji <yujin201800@naver.com>
2022-07-30 06:08:32 +0000

    [ADD]configs.py

---
+++ configs.py
@@ -0,0 +1,17 @@
+save_dir                    = './logs'
+
+miniImageNet_path           	= '../cdfsl_data/miniImagenet'
+miniImageNet_test_path          = '../cdfsl_data/miniImagenet_test'
+tieredImageNet_path           	= '../cdfsl_data/tieredImagenet/train'
+tieredImageNet_test_path        = '../cdfsl_data/tieredImagenet/test'
+DTD_path 						= '/ssd/dtd/images/'
+
+ISIC_path = "../cdfsl_data/ISIC"
+ChestX_path = "../cdfsl_data/chestX"
+CropDisease_path = "../cdfsl_data/CropDiseases"
+EuroSAT_path = "../cdfsl_data/EuroSAT"
+
+cars_path = '../cdfsl_data/cars_cdfsl'
+cub_path = '../cdfsl_data/CUB_200_2011/images'
+places_path = '../cdfsl_data/places_cdfsl'
+plantae_path = '../cdfsl_data/plantae_cdfsl'
